adb2337acdf0d8305507650dc31c2c81897f07e6
Scott Yang <yisyang@gmail.com>
2025-10-12 23:25:23 -0700

    Start xcodeproj and move original Terry code

---
+++ Taspla/TasplaApp.swift
@@ -0,0 +1,32 @@
+//
+//  TasplaApp.swift
+//  Taspla
+//
+//  Created by Scott  Yang on 10/12/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TasplaApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
